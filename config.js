822e8ae6db1f6cc22bd79f0c46ce9ce3065bef84
tinopepino <41546285+tinopepino@users.noreply.github.com>
2018-07-22 12:18:13 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.TOKEN = '';
+exports.TOKEN = 'JcUZHil-1ehx95NFF6By2kX96-_eclFT';
 
-exports.PREFIX = 'm!';
+exports.PREFIX = '}';
 
-exports.GOOGLE_API_KEY = '';
+exports.GOOGLE_API_KEY = 'AIzaSyC2wd5Oxgk1tM8X3pQ8YhrjkroIw2wNyKs';
